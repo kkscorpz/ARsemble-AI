7fa48bb4708cf0766d7a987afbc26e8757474d52
Katelyn Ebuiza <katelyn3161127@gmail.com>
2025-10-12 02:23:34 +0800

    Fix pyyaml dependency conflict

--- build.sh
+++ build.sh
@@ -2,8 +2,5 @@
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# Download spacy model separately if needed
-python -m spacy download en_core_web_md
-
 # Train the Rasa model
 python -m rasa train
