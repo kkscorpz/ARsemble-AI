9687c09b25b10849180f21501cc937947e3955f5
Katelyn Ebuiza <katelyn3161127@gmail.com>
2025-10-12 03:49:56 +0800

    FIX: Use PORT environment variable

--- start.sh
+++ start.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-python -m rasa run \
-  --port $PORT \
-  --credentials credentials.yml \
-  --endpoints endpoints.yml \
-  --enable-api \
-  --cors "*" \
-  --debug
+python -m rasa run --port $PORT --enable-api --cors "*" --debug
