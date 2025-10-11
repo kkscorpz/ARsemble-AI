1afdb240b5904fdd0de2f8ac64271943bb10c0a8
Katelyn Ebuiza <katelyn3161127@gmail.com>
2025-10-12 02:13:55 +0800

    Add build.sh for deployment

--- build.sh
+++ build.sh
@@ -2,5 +2,8 @@
 pip install --upgrade pip
 pip install -r requirements.txt
 
+# Download spacy model separately if needed
+python -m spacy download en_core_web_md
+
 # Train the Rasa model
 python -m rasa train
