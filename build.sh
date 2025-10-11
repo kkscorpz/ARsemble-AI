8bb34e3bf6ceff3891bfc6f818f9b8aaef9cf14c
Katelyn Ebuiza <katelyn3161127@gmail.com>
2025-10-12 02:45:46 +0800

    Try different Rasa version to fix pyyaml build

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-pip install --upgrade pip
-pip install -r requirements.txt
+pip install --upgrade pip setuptools wheel
+
+# Install Rasa first, then other dependencies
+pip install rasa==3.5.15
+pip install rasa-sdk==3.5.2
+
 python -m rasa train
